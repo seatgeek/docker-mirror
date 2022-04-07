d508020ef23f65e80e060a97b418e4cd64ffffba
Kiril Angov <kangov@seatgeek.com>
2022-04-07 19:19:20 -0400

    Remove testing

--- docker_auth.go
+++ docker_auth.go
@@ -12,14 +12,11 @@ import (
 
 func getDockerCredentials(registry string) (*docker.AuthConfiguration, error) {
 	authOptions, err := docker.NewAuthConfigurationsFromDockerCfg()
-
-	log.Info(fmt.Sprintf("AuthOptions:%s", *authOptions))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	creds, ok := authOptions.Configs[registry]
-	log.Info(fmt.Sprintf("Creds:%s", creds))
 	if !ok {
 		return nil, fmt.Errorf("No auth found for %s", registry)
 	}
